7b6447274e70b065d31f3432e8a7ce383d139af0
John Zdanis <zdanis.john@gmail.com>
2014-08-12 20:30:44 -0700

    created a method to convert bad urls to the proper format

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+
+
+  def convert_to_proper_url_format
+
+    bad_url = self.link_url
+
+    if /[www]/ === bad_url
+      bad_url.prepend("http://")
+    else
+      bad_url.prepend("http://www.")
+    end
+    
+
+  end
 end
