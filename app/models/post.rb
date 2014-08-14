244c90a909ebb2eceae26ce4a7851f2a193c8efa
John Zdanis <zdanis.john@gmail.com>
2014-08-13 19:57:27 -0700

    added custom validation for url fomat and now all I need is Rspec to test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,43 @@
 class Post < ActiveRecord::Base
 
 
-  def convert_to_proper_url_format
+  validate :url_is_the_proper_format
 
-    bad_url = self.link_url
 
-    if /[www]/ === bad_url
-      bad_url.prepend("http://")
+  def url_is_valid?
+
+    if /^http:\/\/+www+\.+(\w|\W)+\./ === link_url
+      return true
+    elsif /^https:\/\/+www+\.+(\w|\W)+\./ === link_url
+      return true
     else
-      bad_url.prepend("http://www.")
+      return false
     end
+
+  end
+
+
+  def url_is_the_proper_format
+    if link_url.present? && link_url.start_with?("http://www.")
+      return true
+    else
+      if url_is_valid?
+        self.convert_to_proper_url_format
+      else 
+        errors.add(:link_url, "The URL is not valid.")
+      end
+    end
+  end
+
+
+  def convert_to_proper_url_format
     
+    if /[www]/ === self.link_url
+      self.link_url = self.link_url.prepend("http://")
+    else
+      self.link_url = self.link_url.prepend("http://www.")
+    end
+
 
   end
 end
